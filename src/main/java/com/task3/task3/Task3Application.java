72ed74c211cc10d7417ec192c3e6841b5210dec8
ilusha2274 <ilya.Shamanin@yandex.ru>
2022-06-22 10:30:44 +0300

    first commit

---
+++ src/main/java/com/task3/task3/Task3Application.java
@@ -0,0 +1,15 @@
+package com.task3.task3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Task3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task3Application.class, args);
+	}
+
+}
